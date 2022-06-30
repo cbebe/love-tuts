00768e1a5474efd8d6c2741216ba5e938b2903bf
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-06-29 23:58:07 -0600

    Create a moving player

--- shoot/main.lua
+++ shoot/main.lua
@@ -1,9 +1,16 @@
 function love.load()
   Object = require 'classic'
+  require 'player'
+  love.window.setTitle('Shoot the Enemy')
+  love.window.setMode(500, 500)
+
+  p = Player()
 end
 
 function love.update(dt)
+  p:update(dt)
 end
 
 function love.draw()
+  p:draw()
 end
