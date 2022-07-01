231ff07b1289940302ee99ad1a32647ca2029969
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-07-01 02:12:31 -0600

    Clean up main file

---
+++ agar/game.lua
@@ -0,0 +1,106 @@
+local Object = require 'classic'
+local Entity = require 'entity'
+local Game = Object:extend()
+local lume = require 'lume'
+
+function Game:new(fileName, coinImage, players)
+  self.screenCanvas = love.graphics.newCanvas(400, 600)
+  self.fileName = fileName
+  self.players = players
+  self.coinImage = coinImage
+  self.coins = {}
+  if love.filesystem.getInfo(self.fileName) then
+    self:load()
+  else
+    self:generateCoins()
+  end
+end
+
+function Game:load()
+  local file = love.filesystem.read(self.fileName)
+  local data = lume.deserialize(file)
+  for i=1,#self.players do
+    if data.players[i] ~= nil then
+      self.players[i]:load(data.players[i])
+    end
+  end
+  for i,v in ipairs(data.coins) do
+    self.coins[i] = Entity(v.x, v.y, 10, self.coinImage)
+  end
+end
+
+function Game:generateCoins()
+  for i=1,25 do
+    local coin = Entity(
+      love.math.random(50 ,650),
+      love.math.random(50 ,450),
+      10,
+      self.coinImage
+    )
+    table.insert(self.coins, coin)
+  end
+end
+
+function any(arr, fn)
+  for i,v in ipairs(arr) do
+    if fn(v) then
+      return true
+    end
+  end
+  return false
+end
+
+
+function Game:update(dt)
+  for i,v in ipairs(self.players) do
+    v:update(dt)
+  end
+
+  for i=#self.coins,1,-1 do
+    if any(self.players, function (p)
+      return p:checkCoin(self.coins[i])
+    end) then
+      table.remove(self.coins, i)
+    end
+  end
+  if #self.coins == 0 then
+    self:generateCoins()
+  end
+end
+function Game:save()
+  local data = {}
+  data.players = {}
+  for i,v in ipairs(self.players) do
+    data.players[i] = v:save()
+  end
+  data.coins = {}
+  for i,v in ipairs(self.coins) do
+    data.coins[i] = { x = v.x, y = v.y }
+  end
+  local serialized = lume.serialize(data)
+  love.filesystem.write(self.fileName, serialized)
+end
+
+function Game:reset()
+  love.filesystem.remove(self.fileName)
+  love.event.quit("restart")
+end
+
+function Game:drawPlayerScreen(focus, off)
+  love.graphics.setCanvas(self.screenCanvas)
+    love.graphics.clear()
+    love.graphics.push()
+      local p = self.players[focus]
+      love.graphics.translate(-p.x + 200, -p.y + 300)
+      for i,v in ipairs(self.players) do
+        v:draw()
+      end
+      for i,v in ipairs(self.coins) do
+        v:draw()
+      end
+    love.graphics.pop()
+  love.graphics.setCanvas()
+  love.graphics.draw(self.screenCanvas, off)
+end
+
+return Game
--- agar/main.lua
+++ agar/main.lua
@@ -1,10 +1,6 @@
 function love.load()
-  fileName = 'savedata.txt'
-  lume = require 'lume'
-  Entity = require 'entity'
-  screenCanvas = love.graphics.newCanvas(400, 600)
+  local fileName = 'savedata.txt'
   local Player = require 'player'
-
   local playerImage = love.graphics.newImage('face.png')
   local player1Controls = {
     up = 'up',
@@ -18,94 +14,31 @@ function love.load()
     down = 's',
     right = 'd',
   }
-  player1 = Player(100, 100, 25, playerImage, player1Controls)
-  player2 = Player(300, 100, 25, playerImage, player2Controls)
-
-  coins = {}
-  coinImage = love.graphics.newImage('dollar.png')
-  if love.filesystem.getInfo(fileName) then
-    loadFile(coinImage)
-  else
-    generateCoins(coinImage)
-  end
-end
-
-function loadFile(coinImage)
-  local file = love.filesystem.read(fileName)
-  local data = lume.deserialize(file)
-  player1:load(data.player1)
-  player2:load(data.player2)
-  for i,v in ipairs(data.coins) do
-    coins[i] = Entity(v.x, v.y, 10, coinImage)
-  end
-end
-
-function generateCoins(coinImage)
-  for i=1,25 do
-    local coin = Entity(
-      love.math.random(50 ,650),
-      love.math.random(50 ,450),
-      10,
-      coinImage
-    )
-    table.insert(coins, coin)
-  end
+  local player1 = Player(100, 100, 25, playerImage, player1Controls)
+  local player2 = Player(300, 100, 25, playerImage, player2Controls)
+  local coinImage = love.graphics.newImage('dollar.png')
+  game = require'game'(fileName, coinImage, {player1, player2})
 end
 
 function love.update(dt)
-  player1:update(dt)
-  player2:update(dt)
-
-  for i=#coins,1,-1 do
-    if player1:checkCoin(coins[i]) or player2:checkCoin(coins[i]) then
-      table.remove(coins, i)
-    end
-  end
-  if #coins == 0 then
-    generateCoins(coinImage)
-  end
+  game:update(dt)
 end
 
 function love.draw()
-  drawGame(player1)
-  drawGame(player2, 400)
+  game:drawPlayerScreen(1)
+  game:drawPlayerScreen(2, 400)
   love.graphics.line(400, 0, 400, 600)
-  love.graphics.print('Player 1: ' .. player1.score, 10, 10)
-  love.graphics.print('Player 2: ' .. player2.score, 10, 30)
-end
-
-function drawGame(focus, off)
-  love.graphics.setCanvas(screenCanvas)
-    love.graphics.clear()
-    love.graphics.push()
-      love.graphics.translate(-focus.x + 200, -focus.y + 300)
-      player1:draw()
-      player2:draw()
-      for i,v in ipairs(coins) do
-        v:draw()
-      end
-    love.graphics.pop()
-  love.graphics.setCanvas()
-  love.graphics.draw(screenCanvas, off)
-end
-
-function saveGame()
-  local data = {}
-  data.player1 = player1:save()
-  data.player2 = player2:save()
-  data.coins = {}
-  for i,v in ipairs(coins) do
-    data.coins[i] = { x = v.x, y = v.y }
+  for i=1,#game.players do
+    local line = 'Player ' .. i .. ': ' .. game.players[i].score
+    love.graphics.print(line, 10, 10 + (i - 1) * 20)
   end
-  local serialized = lume.serialize(data)
-  love.filesystem.write(fileName, serialized)
 end
+
 
 function love.keypressed(key)
   if key == 'f1' then
-    saveGame()
+    game:save()
   elseif key == "f2" then
-    love.filesystem.remove(fileName)
-    love.event.quit("restart")
+    game:reset()
   end
 end
